82a12c90d7eab3d0337135c092197d8ce9d72555
lucanwei <lucanwei@user-B9.xrl.com>
2011-09-29 15:46:11 +0800

    增加投保确认页面

---
+++ CheXian/src/com/xrl/chexian/ConfirmApplyInfoActivity.java
@@ -0,0 +1,14 @@
+package com.xrl.chexian;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfirmApplyInfoActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.confirm_apply_info);
+	}
+}
--- CheXian/src/com/xrl/chexian/SaveApplyInfoActivity.java
+++ CheXian/src/com/xrl/chexian/SaveApplyInfoActivity.java
@@ -2,13 +2,18 @@ package com.xrl.chexian;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
 public class SaveApplyInfoActivity extends Activity {
 
 	private TextView textViewTitle;
 	private ImageView imgTitleProgress;
+	private RadioGroup radioGroup;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,30 @@ public class SaveApplyInfoActivity extends Activity {
 		imgTitleProgress = (ImageView) findViewById(R.id.title_progress);
 		imgTitleProgress
 				.setImageResource(R.drawable.save_apply_info_progress);
+		
+		radioGroup = (RadioGroup) findViewById(R.id.save_apply_infi_radio_is_car_own_group);
+		final LinearLayout layoutEmail = (LinearLayout) findViewById(R.id.save_apply_info_email_layout);
+		final LinearLayout layoutMobile = (LinearLayout) findViewById(R.id.save_apply_info_mobile_layout);
+		final LinearLayout layoutIdCardNo = (LinearLayout) findViewById(R.id.save_apply_info_id_card_no_layout);
+		final ImageView line = (ImageView) findViewById(R.id.save_apply_info_after_card_no_line);
+		
+		radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				if(checkedId == R.id.save_apply_info_radio_is_car_own_yes){
+					line.setVisibility(View.GONE);
+					layoutEmail.setVisibility(View.GONE);
+					layoutMobile.setVisibility(View.GONE);
+					layoutIdCardNo.setBackgroundResource(R.drawable.query_model_bottom);
+				}else{
+					line.setVisibility(View.VISIBLE);
+					layoutIdCardNo.setBackgroundResource(R.drawable.query_model_middle);
+					layoutMobile.setVisibility(View.VISIBLE);
+					layoutEmail.setVisibility(View.VISIBLE);
+				}
+			}
+		});
 	}
 	
 }
